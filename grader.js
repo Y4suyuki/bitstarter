18c9ee319fb32bbba94eda940f273e0f50a6311e
Yasuyuki Ageishi <age15h1@me.com>
2013-07-12 10:58:43 +0000

    modified grader.js to handle url

--- grader.js
+++ grader.js
@@ -23,6 +23,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -32,12 +33,23 @@ var assertFileExists = function(infile) {
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    console.log('file found!');
     return instr;
+};
+
+var assertURLExists = function(url) {
+    console.log(url);
+    rest.get(url).on("error", function(err){
+	console.log("error occured");
+	process.exit(1);
+    });
+    return url;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -54,6 +66,23 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(url, checksfile) {
+    rest.get(url).on("complete", function(result, response) {
+	$ = cheerio.load(result);
+	var checks= loadChecks(checksfile).sort();
+	var out={};
+	for(var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	}
+
+	console.log(out);
+        var outJson = JSON.stringify(out, null, 4);
+	fs.writeFileSync("out.json", outJson);
+    });
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,8 +93,17 @@ if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>','URL to file', clone(assertURLExists))  
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    if (program.url) {
+	console.log('program.url is true');
+	console.log(program.url)
+	var checkJson = checkURL(program.url, program.checks);
+    } else if(program.file) {
+	console.log('program.url is false');
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
